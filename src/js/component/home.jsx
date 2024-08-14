f248d6c9f6c97c5481fbd40b2856d1a9250d6354
Herbert7365 <161256945+Herbert7365@users.noreply.github.com>
2024-08-14 23:23:35 +0000

    1er commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,16 @@
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 
 //create your first component
 // Funcion flecha
 const Home = () => {
 	// Estados para almacenar la lista de tareas y el nuevo todo a agregar
 	const [nuevoTodo, setNuevoTodo] = useState("");
-	const [todos, setTodos] = useState(["mi tarea 1", "mi tarea 2"]);
+	const [todos, setTodos] = useState([]);
+
+
+	const [name, setName]= useState("");
+	const [usersList, setUsersList]=useState([]);
+
 
 	// Manejador del evento onChange del input
  	// Actualiza el estado `nuevoTodo` con el valor ingresado por el usuario
@@ -29,13 +34,53 @@ const Home = () => {
 		}
 	}
 
+	// ------------- FETCH ------------------------
+
+	function crearUsuario() {
+
+		fetch(`https://playground.4geeks.com/todo/users/${name}`, {
+			method: 'POST'
+
+		})
+		.then((response) => response.json())
+		.then((data)=>{
+			if (data.id) {
+				alert("Usuario creado con exito")
+			} else {
+				alert("Upps algop salio mal")
+			}
+		})
+		.catch((error)=>console.log(error))
+
+	}
+	
+	const getUsers=()=>{
+		fetch("https://playground.4geeks.com/todo/users")
+		.then((response)=>{response.json()})
+		.then((data)=>{setUsersList(data.users)})
+	}
+
+	useEffect(()=>{
+		getUsers()
+	},[])
+
 	return (
 		<div className="text-center">
+			<input type="text" onChange={(e)=>setName(e.target.value)} />
+			<button onClick={crearUsuario}>Crear Usuario</button>
+			<label>Lista de usuarios</label>
+			{
+				usersList.map((item,index)=>{
+					return(
+						<h5 key={index}>{item.name}</h5>
+					)
+				})
+			}
 			<h1 className="text-center mt-5">
 			Todos
 			</h1>
-			<div className="container bsDanger text-left shadow p-3 mb-5 bg-body rounded">
-				<input type="text" className="form-control" onChange={handleChange} onKeyDown={handlerEnter} value={nuevoTodo}/>
+			<div className="container bsDanger text-left shadow p-3 mb-5 bg-body rounded lead">
+				<input type="text" className="form-control" onChange={handleChange} onKeyDown={handlerEnter} value={nuevoTodo} placeholder="What needs to be done?"/>
 				<div className="text-left">
 					<ul className="list-group">
 						{/* Elementos de la lista */}
